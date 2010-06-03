373b4babcd0b46748b34f2f4373d07028aec6102
Robey Pointer <robey@twitter.com>
2010-06-02 18:37:25 -0700

    update definitions of 'get' and 'get_range'.

--- src/main/thrift/TimelineStore.thrift
+++ src/main/thrift/TimelineStore.thrift
@@ -5,8 +5,8 @@ namespace ruby Haplocheirus
 service TimelineStore {
   void append(1: binary entry, 2: list<string> timeline_ids);
   void remove(1: binary entry, 2: list<string> timeline_ids);
-  list<binary> get(1: string timeline_id, 2: i32 offset, 3: i32 length);
-  list<binary> get_range(1: string timeline_id, 2: i64 from_id, 3: i64 to_id);
+  list<binary> get(1: string timeline_id, 2: i32 offset, 3: i32 length, 4: bool dedupe);
+  list<binary> get_since(1: string timeline_id, 2: i64 from_id, 3: bool dedupe);
   void store(1: string timeline_id, 2: list<binary> entries);
   void merge(1: string timeline_id, 2: list<binary> entries);
   void unmerge(1: string timeline_id, 2: list<binary> entries);
