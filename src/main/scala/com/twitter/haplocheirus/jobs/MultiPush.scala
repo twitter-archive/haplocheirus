d9e1f6b9e1f7d36ce515b11680387b876578e5d3
Robey Pointer <robey@twitter.com>
2010-09-21 00:03:19 -0700

    try skipping the tedious quoting in json for blobs.

--- src/main/scala/com/twitter/haplocheirus/jobs/MultiPush.scala
+++ src/main/scala/com/twitter/haplocheirus/jobs/MultiPush.scala
@@ -4,20 +4,21 @@ import com.twitter.gizzard.jobs.{UnboundJob, UnboundJobParser}
 import com.twitter.gizzard.nameserver.NameServer
 import com.twitter.gizzard.scheduler.JobScheduler
 import com.twitter.gizzard.thrift.conversions.Sequences._
+import com.twitter.json.JsonQuoted
 import org.apache.commons.codec.binary.Base64
 
 object MultiPushParser extends UnboundJobParser[(NameServer[HaplocheirusShard], JobScheduler)] {
   def apply(attributes: Map[String, Any]) = {
-    new MultiPush(Base64.decodeBase64(attributes("entry").asInstanceOf[String]),
+    new MultiPush(Base64.decodeBase64(attributes("entry").toString),
                   attributes("timeline_prefix").asInstanceOf[String],
-                  Base64.decodeBase64(attributes("timeline_ids").asInstanceOf[String]).toLongArray)
+                  Base64.decodeBase64(attributes("timeline_ids").toString).toLongArray)
   }
 }
 
 case class MultiPush(entry: Array[Byte], timelinePrefix: String, timelineIds: Seq[Long])
      extends UnboundJob[(NameServer[HaplocheirusShard], JobScheduler)] with JobInjector {
   protected def encodeBase64(data: Array[Byte]) = {
-    Base64.encodeBase64String(data).replaceAll("\r\n", "")
+    JsonQuoted("\"" + Base64.encodeBase64String(data).replaceAll("\r\n", "") + "\"")
   }
 
   def toMap = {
