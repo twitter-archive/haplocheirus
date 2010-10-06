9e15dc021c0cd89f9942c93fc83ca146f64514f8
Robey Pointer <robey@twitter.com>
2010-10-05 19:44:48 -0700

    start of a multipush thrift codec. not actually filled in yet.

--- src/main/scala/com/twitter/haplocheirus/jobs/MultiPush.scala
+++ src/main/scala/com/twitter/haplocheirus/jobs/MultiPush.scala
@@ -1,11 +1,30 @@
 package com.twitter.haplocheirus.jobs
 
 import com.twitter.gizzard.nameserver.NameServer
-import com.twitter.gizzard.scheduler.{JobScheduler, JsonCodec, JsonJob, JsonJobParser}
+import com.twitter.gizzard.scheduler.{Codec, JobScheduler, JsonCodec, JsonJob, JsonJobParser}
 import com.twitter.gizzard.thrift.conversions.Sequences._
 import com.twitter.json.JsonQuoted
 import org.apache.commons.codec.binary.Base64
+import thrift.TimelineStore
 
+class MultiPushCodec(nameServer: NameServer[HaplocheirusShard], scheduler: JobScheduler[JsonJob])
+      extends Codec[MultiPush] {
+  def flatten(job: MultiPush): Array[Byte] = {
+    //
+    null
+  }
+
+  def inflate(data: Array[Byte]): MultiPush = {
+    //
+    val args = new TimelineStore.append_args()
+    val entry = "".getBytes
+    val timelinePrefix = ""
+    val timelineIds = Array(0L)
+    new MultiPush(entry, timelinePrefix, timelineIds, nameServer, scheduler)
+  }
+}
+
+// temp for backward compat
 class MultiPushParser(nameServer: NameServer[HaplocheirusShard], scheduler: JobScheduler[JsonJob]) extends JsonJobParser[JsonJob] {
   def apply(codec: JsonCodec[JsonJob], attributes: Map[String, Any]) = {
     new MultiPush(Base64.decodeBase64(attributes("entry").toString),
@@ -15,11 +34,10 @@ class MultiPushParser(nameServer: NameServer[HaplocheirusShard], scheduler: JobS
   }
 }
 
+
 case class MultiPush(entry: Array[Byte], timelinePrefix: String, timelineIds: Seq[Long],
                      nameServer: NameServer[HaplocheirusShard], scheduler: JobScheduler[JsonJob])
      extends JsonJob with JobInjector {
-  type Environment = (NameServer[HaplocheirusShard], JobScheduler[JsonJob])
-
   protected def encodeBase64(data: Array[Byte]) = {
     JsonQuoted("\"" + Base64.encodeBase64String(data).replaceAll("\r\n", "") + "\"")
   }
