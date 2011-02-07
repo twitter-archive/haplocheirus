cd91dfb17e53b34d7c12f44583463715152d32e8
Attila Szegedi <szegedia@gmail.com>
2011-02-07 11:05:17 -0800

    Update to newest 1.5.2-a Gizzard amd 1.2.15 Ostrich

--- project/build/HaplocheirusProject.scala
+++ project/build/HaplocheirusProject.scala
@@ -2,12 +2,12 @@ import sbt._
 import com.twitter.sbt._
 
 class HaplocheirusProject(info: ProjectInfo) extends StandardProject(info) with GithubPublisher {
-  val gizzard = "com.twitter" % "gizzard" % "1.5.12"
+  val gizzard = "com.twitter" % "gizzard" % "1.5.2-a"
   val jredis = "jredis" % "jredis" % "1.0-tw1"
   val codec = "commons-codec" % "commons-codec" % "1.4"
   val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"  //--auto--
 
-  val ostrich = "com.twitter" % "ostrich" % "1.2.14"
+  val ostrich = "com.twitter" % "ostrich" % "1.2.15"
 
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   val jmock = "org.jmock" % "jmock" % "2.4.0" % "test"  //--auto--
