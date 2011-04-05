91c5004c8107aeed1035585277ecd8fcd891820f
Brandon Mitchell <brandon@twitter.com>
2011-04-05 11:40:47 -0700

    Well, that's fucking dumb.

--- src/main/scala/com/twitter/haplocheirus/PipelinedRedisClient.scala
+++ src/main/scala/com/twitter/haplocheirus/PipelinedRedisClient.scala
@@ -175,7 +175,7 @@ class PipelinedRedisClient(hostname: String, pipelineMaxSize: Int, timeout: Dura
         } else {
           redisClient.rpushx(tempName, entry)
         }
-      }
+      }.projection.force
       // All we care is that they all completed, not each individual one
       futures.lastOption.foreach { future =>
         future.get(timeout.inMillis, TimeUnit.MILLISECONDS)
