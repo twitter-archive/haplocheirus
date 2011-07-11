60c63bf3a1033ed9cdac7ddbdb16768d39c391d0
Brandon Mitchell <brandon@twitter.com>
2011-07-11 16:17:35 -0700

    oops.

--- src/scripts/setup-env.sh
+++ src/scripts/setup-env.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION="1.0"
+VERSION="@VERSION@"
 
 CONFIG=./config/development.scala
 JAR=./dist/haplocheirus/haplocheirus-${VERSION}.jar
